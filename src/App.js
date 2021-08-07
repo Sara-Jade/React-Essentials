20419b1f78843e2e6639ccf6b007ab8983b50f4e
SuperJuice <wisconsinsara@gmail.com>
2021-08-07 16:51:24 -0700

    Intro, components, component tree, passing props, jsx, babel, displaying lists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
+const Header = (props) => {
+  return (
+    <>
+    <h1>{props.name}'s Atomic Kitchen</h1>
+    <h2>Dessert is a nuclear experience!</h2>
+    </>
+  );
+}
+
+const Main = () => {
+  return (
+    <section>
+      <img src={logo} alt="React logo"/>
+    </section>
+  );
+}
+
+const Footer = (props) => {
+  return (
+    <footer>
+      <p>{props.year}</p>
+      <p>About the scientist chef</p>
+    </footer>
+  )
+}
+
+const menuItems = [
+  'NDT chocolate mousse',
+  'Peanut butter and chocolate chip fusion cookies',
+  'Curie\'s creamy frosted cupcakes'
+];
+
+const menuItemsObj = menuItems.map((menuItem, index) => ({id: index, title: menuItem}));
+
+const Menu = () => {
+  return (
+    <>
+    <h3>Menu</h3>
+    <ul style={{textAlign: 'left'}}>
+      {menuItemsObj.map((menuItem) => <li key={menuItem.id}>{menuItem.title}</li>)}
+    </ul>
+    </>
+  );
+}
+
+const App = () => {
+  const year = new Date().getFullYear();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header name = 'Tara'/>
+     <Main />
+     <Menu />
+     <Footer year={year}/>
     </div>
   );
 }
