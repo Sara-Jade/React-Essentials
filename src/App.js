67ea4b3da0379a852914a414e171a7f37f91cf92
SuperJuice <wisconsinsara@gmail.com>
2021-08-16 12:08:33 -0700

    fetch: handle success, loading, and fail states.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { useEffect, useState } from 'react';
 
 const App = (props) => {
   const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
+    setLoading(true);
     fetch(`https://api.agify.io?name=${props.firstName}`)
     .then(response => response.json())
-    .then(setData);
+    .then(setData)
+    .then(setLoading(false))
+    .catch(error => setError(error));
   }, [props.firstName]);
 
   const [gitHubData, setGitHubData] = useState(null);
@@ -15,7 +21,11 @@ const App = (props) => {
     .then(response => response.json())
     .then(setGitHubData);
   }, [props.username]);
-   
+  
+  if (isLoading) return <p>{props.firstName} data is loading.</p>
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if (!data) return <p>Sorry! We don't have data on ${props.firstName}</p>
+
   return(
     <>
       <p>{data 
