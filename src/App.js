a7e99f80d0213d18feec1ab1f9d493054aabefd1
SuperJuice <wisconsinsara@gmail.com>
2021-08-15 22:34:58 -0700

    fetched data from 2nd api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
-const App = ({firstName}) => {
+const App = (props) => {
   const [data, setData] = useState(null);
-
   useEffect(() => {
-    fetch(`https://api.agify.io?name=${firstName}`)
+    fetch(`https://api.agify.io?name=${props.firstName}`)
     .then(response => response.json())
     .then(setData);
-  }, [firstName]);
+  }, [props.firstName]);
+
+  const [gitHubData, setGitHubData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${props.username}`)
+    .then(response => response.json())
+    .then(setGitHubData);
+  }, [props.username]);
    
-  return data ? <p>{JSON.stringify(data)}</p> : <p>No data on {firstName}</p>;
-};
+  return(
+    <>
+      <p>{data 
+        ? JSON.stringify(data) : `No data on ${props.firstName}`}</p>
+      <p>{gitHubData ? JSON.stringify(gitHubData) : `No data on ${props.username}`}</p>
+    </>
+  )};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App firstName='Lewis'/>,
+  <App 
+    firstName='Lewis'
+    username='Sara-Jade'/>,
   document.getElementById('root')
 );
 
